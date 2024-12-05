87dba933c35d38450796aced12113401f36d3438
sebastianVP <alex.valdezp22@gmail.com>
2024-12-04 20:50:35 -0500

    se añadio el metodo restar

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,9 @@
 def sumar(a,b):
     return a+b
 
-
-
+def restar(a,b):
+    return a-b
 
 print("Resultados iniciales en developer")
 print("La suma de 3 + 5 es:",sumar(3,5))
+print("La resta de 10 - 6 es:",restar(10,6))
