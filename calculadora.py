5f42d895229e9b1ec5b95b85b9c4e8884d75b590
sebastianVP <alex.valdezp22@gmail.com>
2024-12-04 20:54:20 -0500

    metodo nuevo para multiplicar

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,9 @@ def sumar(a,b):
     return a+b
 
 
-
+def multiplicar(a,b):
+    return a*b
 
 print("Resultados iniciales en developer")
 print("La suma de 3 + 5 es:",sumar(3,5))
+print("La multiplicacion de 4*6 es:",multiplicar(4,6))
